8738e1fd174e2cc4c28367f0dd7d4c9648a3afce
Nicole Holder <holdern@wwt.com>
2020-08-30 18:30:19 -0500

    Added material-UI WIP on User Component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { store } from './actions/store';
 import { Provider } from 'react-redux';
 import User from './components/User'
 import Usersform from './components/Usersform'
+import { Container} from "@material-ui/core"
 
 function App() {
   return (
     <Provider store = {store}>
+      <Container maxWidth="lg"></Container>
       <User/>
       <Usersform/>
       </Provider>
--- src/components/user.js
+++ src/components/user.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions/user";
+import { Grid, Paper, TableContainer, Table, TableHead, TableRow, TableCell, TextField } from "@material-ui/core";
+import UsersForm from "./Usersform";
 
 const User = (props) => {
 //    const [x, setX]= useState(0)
@@ -11,7 +13,26 @@ props.fetchAllUsers()
     }, [])
 
     return (
-        <div>from users</div>);
+    //     <Paper>
+    //     {<Grid container>
+    //         <Grid item xs={6}>
+    //             <UsersForm />
+    //         </Grid>
+    //         <Grid item xs={6}>
+    //             <TableContainer>
+    //                 <Table>
+    //                     <TableHead>
+    //                         <TableRow>
+    //                             <TableCell>Name</TableCell>
+    //                             <TableCell>Email</TableCell>
+    //                         </TableRow>
+    //                     </TableHead>
+    //                 </Table>
+    //             </TableContainer>
+    //         </Grid>
+    // </Grid>}
+    //     </Paper>
+    );
 }
 
 const mapStateToProps = state => ({
