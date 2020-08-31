89d4d6d45f9e3ade6a5ea1a815440844be142839
Nicole Holder <holdern@wwt.com>
2020-08-31 18:33:00 -0500

    added useForm hook

--- src/components/usersform.js
+++ src/components/usersform.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { TextField, Grid } from '@material-ui/core';
+import useForm from "./useForm"
+
+
+const initialUserValues={
+    fullName : '',
+    email: ''
+}
 
 const UsersForm = (props) => {
-    return ( <div> Pet Owners </div> )
+
+    const {
+            values,
+            setValues,
+            handleInputChange
+    } = useForm(initialUserValues)
+
+    return (
+        <form autoComplete="off" noValidate>
+            <Grid container>
+                <Grid item xs={6}>
+                    <TextField
+                    name="fullName"
+                    variant="outlined"
+                    label="Full Name"
+                    value={initialUserValues.fullName}
+                    onChange={handleInputChange}
+                    />
+                    <TextField
+                    name="email"
+                    variant="outlined"
+                    label="Email Address"
+                    value={initialUserValues.email}
+                    onChange={handleInputChange}
+                    />
+                    <TextField
+                    name="password"
+                    variant="outlined"
+                    label="Password"
+                    />
+                </Grid>
+            </Grid>
+        </form>
+    )
 }
 
 export default UsersForm;
